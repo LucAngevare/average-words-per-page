51a92405c269917711ac44ba41f088a53f1d6c0b
Luc Angevare <lucangevare@gmail.com>
2022-03-25 20:47:41 +0100

    Finished running project overday. Results coming

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import json, xlsxwriter, re, os, subprocess as subp, time
+import json, re, os, subprocess as subp, time, openpyxl
 from datetime import datetime
 from docx import Document
 from PyPDF2 import PdfFileReader
 
 dictionary = json.load(open("index.json"))
-workbook = xlsxwriter.Workbook("data.xlsx")
-worksheet = workbook.add_worksheet()
 
 def count_chars(doc):
     newparatextlist = []
@@ -15,8 +13,10 @@ def count_chars(doc):
     return len(re.findall(r'\w+', '\n'.join(newparatextlist)))
 
 for dictionary_iteration in range(10000): #going for 1.000 iterations here just to see exactly how long this takes, if all goes well and stuff I'll up it, calculate how long each iteration takes exactly and I guess run the simulations overnight
-    for word_amount in range(300, 600): #using a starting point of 300 words because on average 500 words fills a page so I doubt any less will do, it's always possible though
+    for word_amount in range(300, 700): #using a starting point of 300 words because on average 500 words fills a page so I doubt any less will do, it's always possible though, a maximum of 700 because why not, it'll start the next dictionary_iteration when it has 2 pages
         doc = Document()
+        wb = openpyxl.load_workbook("data.xlsx")
+        worksheet = wb["Sheet1"]
         startingTime = datetime.now()
         text = " ".join(dictionary[dictionary_iteration:dictionary_iteration+word_amount])
         paragraph = doc.add_paragraph(text)
@@ -25,15 +25,15 @@ for dictionary_iteration in range(10000): #going for 1.000 iterations here just 
         with open('sim.pdf', 'rb') as f:
             pdf = PdfFileReader(f)
             pageNum = pdf.getNumPages()
-
-        worksheet.write(f'A{dictionary_iteration}', dictionary_iteration)
-        if (count_chars(doc)==word_amount): worksheet.write(f'B{dictionary_iteration}', count_chars(doc))
+        maximum_row = worksheet.max_row+1
+        worksheet[f'A{maximum_row}'] = dictionary_iteration
+        if (count_chars(doc)==word_amount): worksheet[f'B{maximum_row}'] = count_chars(doc)
         else:
             print(f'{count_chars(doc)} not equal to {word_amount}!')
-            worksheet.write(f'B{dictionary_iteration}', (count_chars(doc)+word_amount)/2) #if something does go wrong and the actual word count in the document that is read in within the data is not equal to the theoretical word count, it should just take the average as that probably won't have too much impact on the simulation we're running, I will log this to the console, so if it happens constantly, something consistently doesn't add up, and more research is required.
-        worksheet.write(f'C{dictionary_iteration}', pageNum)
-        worksheet.write(f'D{dictionary_iteration}', startingTime.microsecond-datetime.now().microsecond)
-        worksheet.write(f'E{dictionary_iteration}', len(text))
+            worksheet[f'B{maximum_row}'] = (count_chars(doc)+word_amount)/2 #if something does go wrong and the actual word count in the document that is read in within the data is not equal to the theoretical word count, it should just take the average as that probably won't have too much impact on the simulation we're running, I will log this to the console, so if it happens constantly, something consistently doesn't add up, and more research is required.
+        worksheet[f'C{maximum_row}'] = pageNum
+        worksheet[f'D{maximum_row}'] = datetime.now().microsecond-startingTime.microsecond
+        worksheet[f'E{maximum_row}'] = len(text)
         print(f'{dictionary_iteration} with a length of {word_amount} has {pageNum} pages, and {len(text)} characters.')
         result = False
         os.remove("sim.docx")
@@ -43,5 +43,8 @@ for dictionary_iteration in range(10000): #going for 1.000 iterations here just 
                 result = True
             except:
                 time.sleep(1)
-
-workbook.close()
+        wb.save("data.xlsx")
+        if (pageNum == 2):
+            break
+    else:
+        continue
